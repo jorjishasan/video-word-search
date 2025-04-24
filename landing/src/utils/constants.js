039132d990321a4fe24d74241cfc573e48bdd9cd
jorjishasan <dev.jorjis@gmail.com>
2025-04-24 18:58:55 +0600

    feat: export socials object for improved accessibility in other modules

--- landing/src/utils/constants.js
+++ landing/src/utils/constants.js
@@ -1,6 +1,9 @@
 export const CHROME_EXTENSION_LINK_FROM_WEBSTORE = 'https://chrome.google.com/webstore/detail/ai-assistant/';
 
-const socials = {
+export const socials = {
     twitter: 'https://twitter.com/your_twitter_handle',
+    github:'https://github.com/uiforest',
+    discord: 'https://discord.gg/your_discord_link',
+    youtube: 'https://www.youtube.com/channel/your_youtube_channel',
 
 }
