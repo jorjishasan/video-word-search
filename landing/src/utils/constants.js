22ad0433ee3ff49fa09823a1946d9c637422334b
Jorjis Hasan <dev.jorjis@gmail.com>
2025-05-14 04:46:03 +0600

    Update extension's public URL

--- landing/src/utils/constants.js
+++ landing/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const CHROME_EXTENSION_LINK_FROM_WEBSTORE = 'https://chrome.google.com/webstore/detail/ai-assistant/';
+export const CHROME_EXTENSION_LINK_FROM_WEBSTORE = 'https://chromewebstore.google.com/detail/video-word-search/inaclbpneccimkjbcibeppkfagkhidfa';
 
 export const socials = {
     twitter: 'https://twitter.com/your_twitter_handle',
